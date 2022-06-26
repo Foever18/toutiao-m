09edadaf185ae5cc93f11cef8bce14aa3bfea09b
Foever18 <1012163589@qq.com>
2022-06-26 11:44:35 +0800

    路由配置完成

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './style/index.less'
+// 引入icon
+import '@/style/icon.less'
 // 引入vant
 import Vant from 'vant'
 import 'vant/lib/index.css'
+// rem适配
 import 'amfe-flexible'
 Vue.use(Vant)
 Vue.config.productionTip = false
---
+++ src/views/Layout/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <router-view></router-view>
+    <van-tabbar route>
+      <van-tabbar-item icon="home-o" replace to="/home"
+        >首页
+        <i class="toutiao toutiao-shouye" slot="icon"></i>
+      </van-tabbar-item>
+      <van-tabbar-item icon="search" replace to="/video"
+        >视频
+        <i class="toutiao toutiao-shipin" slot="icon"></i>
+      </van-tabbar-item>
+      <van-tabbar-item icon="friends-o" replace to="/question"
+        >问答
+        <i class="toutiao toutiao-wenda" slot="icon"></i>
+      </van-tabbar-item>
+      <van-tabbar-item icon="setting-o" replace to="/my"
+        >我的
+        <i class="toutiao toutiao-wode" slot="icon"></i>
+      </van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script scoped >
+export default {
+
+}
+</script>
+
+<style scoped lang='less'>
+.toutiao {
+  font-size: 40px;
+}
+/deep/ .van-tabbar-item__text {
+  font-size: 20px !important;
+}
+</style>
